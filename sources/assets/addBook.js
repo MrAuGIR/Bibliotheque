cd3614b91df0f0566ebb89a957e526a3eb9e63ea
MrAuGIR <augirard17@gmail.com>
2025-04-06 21:31:15 +0200

    refacto

--- sources/assets/addBook.js
+++ sources/assets/addBook.js
@@ -1,80 +1,79 @@
 
-function removeApiBookFromBiblio(){
-    let links = document.querySelectorAll("[js-link-remove]");
+function removeApiBookFromBiblio() {
+  let links = document.querySelectorAll("[js-link-remove]");
 
-    for (let link of links){
+  for (let link of links) {
 
-        link.addEventListener('click',async function(e){
-            e.preventDefault();
+    link.addEventListener('click', async function(e) {
+      e.preventDefault();
 
-            let idBook = this.getAttribute('book-id');
+      let idBook = this.getAttribute('book-id');
 
-            const data = await removeBook(idBook);
+      const data = await removeBook(idBook);
 
-            removeHtmlElement('card-'+idBook);
-        });
-    }
+      removeHtmlElement('card-' + idBook);
+    });
+  }
 }
 
-function addApiBookToBiblio (){
-    let links = document.querySelectorAll("[js-link-add]");
+function addApiBookToBiblio() {
+  let links = document.querySelectorAll("[js-link-add]");
 
-    for( let link of links){
-        link.addEventListener('click', async function(e){
-            e.preventDefault()
+  for (let link of links) {
+    link.addEventListener('click', async function(e) {
+      e.preventDefault()
 
-            const divResult = document.getElementById('query-result');
-            const p = divResult.querySelector('p');
+      const divResult = document.getElementById('query-result');
+      const p = divResult.querySelector('p');
 
-            let idBook = this.getAttribute('book-id');
-            let result = await addBook(idBook);
-            console.log(result)
-        });
-    }
+      let idBook = this.getAttribute('book-id');
+      await addBook(idBook);
+    });
+  }
 }
 
 const addBook = async (apiId) => {
 
-    const res = await fetch("/book/add",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({id:apiId})
-    })
+  const res = await fetch("/book/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: apiId })
+  })
 
-    if (res.ok) {
-        return await res.json();
-    }
-    return undefined;
+  if (res.ok) {
+    return await res.json();
+  }
+  return undefined;
 }
 
 const removeBook = async (id) => {
 
-    const res = await fetch("/book/"+id,{
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({id:id})
-    })
+  const res = await fetch("/book/" + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: id })
+  })
 
-    if (res.ok) {
-        return await res.json();
-    }
-    return undefined;
+  if (res.ok) {
+    return await res.json();
+  }
+  return undefined;
 }
 
 const removeHtmlElement = (id) => {
-    const el = document.getElementById(id);
-    if (el !== undefined) {
-        el.innerHTML = ''
-    }
+  const el = document.getElementById(id);
+  if (el !== undefined) {
+    el.innerHTML = ''
+  }
 }
 
 window.onload = () => {
-    removeApiBookFromBiblio()
-    addApiBookToBiblio()
+  removeApiBookFromBiblio()
+  addApiBookToBiblio()
 }
 
 
--- sources/src/Controller/BookController.php
+++ sources/src/Controller/BookController.php
@@ -4,9 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Biblio;
 use App\Entity\Book;
-use App\Entity\Comment;
 use App\Entity\User;
-use App\Form\CommentType;
 use App\Service\Api\GoogleBook;
 use App\Service\Api\Input\FecthInputDto;
 use App\Service\Api\Input\SearchInputDto;
@@ -26,83 +24,78 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 #[Route("/book", name: "book_")]
 class BookController extends AbstractController
 {
-    public function __construct(
-        private readonly GoogleBook $googleBook,
-        private readonly EntityManagerInterface $entityManager
-    )
-    {
+  public function __construct(
+    private readonly GoogleBook $googleBook,
+    private readonly EntityManagerInterface $entityManager
+  ) {}
+
+  /**
+   * @throws TransportExceptionInterface
+   * @throws ServerExceptionInterface
+   * @throws RedirectionExceptionInterface
+   * @throws ClientExceptionInterface
+   */
+  #[Route('/{id}/show', name: 'show', methods: [Request::METHOD_GET, Request::METHOD_POST])]
+  public function show(string $id, Request $request): Response
+  {
+    $apiBook = $this->googleBook->get($id);
+
+    $book = $this->entityManager->getRepository(Book::class)->findOneBy(['apiId' => $id]);
+
+    $dto = new SearchInputDto('+inauthor:' . $apiBook->getVolumeInfo()->getAuthors()[0], 9);
+    $relatedBooks = $this->googleBook->list($dto);
+
+
+    return $this->render('book/show.html.twig', [
+      'book' => $apiBook,
+      'booksRelated' => $relatedBooks,
+      'entityId' => $book?->getId() ?? null
+
+    ]);
+  }
+
+  #[Route('/search', name: "search", methods: [Request::METHOD_GET, Request::METHOD_POST])]
+  public function search(): Response
+  {
+
+    return $this->render('book/search.html.twig', []);
+  }
+
+  #[Route("/add", name: "add_to_biblio", methods: [Request::METHOD_POST])]
+  public function addBook(
+    #[MapRequestPayload] FecthInputDto $fecthInputDto,
+    Loader $loader
+  ): JsonResponse {
+    $apiBook = $this->googleBook->get($fecthInputDto->getId());
+
+    $book = $loader->load($apiBook);
+
+    $biblio = $this->loadUserBiblio();
+
+    $biblio->addBook($book);
+
+    $this->entityManager->persist($biblio);
+    $this->entityManager->flush();
+
+    return $this->json(['status' => 'book added']);
+  }
+
+  #[Route("/{id}", name: "remove_to_biblio", methods: [Request::METHOD_DELETE])]
+  public function removeBook(string $id): JsonResponse
+  {
+    if (!empty($book = $this->entityManager->getRepository(Book::class)->findOneBy(['id' => $id]))) {
+      $biblio = $this->loadUserBiblio();
+      $biblio->removeBook($book);
+      $this->entityManager->persist($biblio);
+      $this->entityManager->flush();
     }
+    return $this->json([]);
+  }
 
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
-     */
-    #[Route('/{id}/show', name: 'show', methods: [Request::METHOD_GET,Request::METHOD_POST])]
-    public function show(string $id,Request $request): Response
-    {
-        $apiBook = $this->googleBook->get($id);
-
-        $book = $this->entityManager->getRepository(Book::class)->findOneBy(['apiId' => $id]);
-
-        $dto = new SearchInputDto('+inauthor:'.$apiBook->getVolumeInfo()->getAuthors()[0],9);
-        $relatedBooks = $this->googleBook->list($dto);
-
-
-        return $this->render('book/show.html.twig', [
-            'book' => $apiBook,
-            'booksRelated' => $relatedBooks,
-            'entityId' => $book?->getId() ?? null
-
-        ]);
-    }
-
-    #[Route('/search', name: "search", methods: [Request::METHOD_GET, Request::METHOD_POST])]
-    public function search(): Response
-    {
-
-        return $this->render('book/search.html.twig',[
-
-        ]);
-    }
-
-    #[Route("/add", name: "add_to_biblio", methods: [Request::METHOD_POST])]
-    public function addBook(
-        #[MapRequestPayload] FecthInputDto $fecthInputDto,
-        Loader $loader
-    ): JsonResponse
-    {
-        $apiBook = $this->googleBook->get($fecthInputDto->getId());
-
-        $book = $loader->load($apiBook);
-
-        $biblio = $this->loadUserBiblio();
-
-        $biblio->addBook($book);
-
-        $this->entityManager->persist($biblio);
-        $this->entityManager->flush();
-
-        return $this->json($book);
-    }
-
-    #[Route("/{id}", name: "remove_to_biblio", methods: [Request::METHOD_DELETE])]
-    public function removeBook(string $id,Request $request): JsonResponse
-    {
-        if (!empty($book = $this->entityManager->getRepository(Book::class)->findOneBy(['id' => $id]))) {
-            $biblio = $this->loadUserBiblio();
-            $biblio->removeBook($book);
-            $this->entityManager->persist($biblio);
-            $this->entityManager->flush();
-        }
-        return $this->json([]);
-    }
-
-    private function loadUserBiblio(): Biblio
-    {
-        /** @var User $user */
-        $user = $this->getUser();
-        return $user->getBiblio();
-    }
+  private function loadUserBiblio(): Biblio
+  {
+    /** @var User $user */
+    $user = $this->getUser();
+    return $user->getBiblio();
+  }
 }
