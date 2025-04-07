2f147483e002dca7fcf22a1f3a922a58052fe5d9
MrAuGIR <augirard17@gmail.com>
2025-04-07 23:35:07 +0200

    mise a jour crud comment

--- sources/src/Controller/CommentController.php
+++ sources/src/Controller/CommentController.php
@@ -5,58 +5,62 @@ namespace App\Controller;
 use App\Entity\Book;
 use App\Entity\Comment;
 use App\Form\CommentType;
+use App\Service\Comment\EditCommentDto;
+use App\Service\Comment\Loader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/comment', name: 'comment_')]
 class CommentController extends AbstractController
 {
-    public function __construct(
-        private EntityManagerInterface $entityManager
-    )
-    {
+  public function __construct(
+    private EntityManagerInterface $entityManager
+  ) {}
+
+  #[Route('/book/{id}', name: 'book', methods: ['GET', 'POST'])]
+  public function list(Book $book, Request $request): Response
+  {
+    $comment = new Comment();
+    $form = $this->createForm(CommentType::class, $comment, [
+      'action' => $this->generateUrl('comment_book', ['id' => $book->getId()]),
+    ]);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $comment->setBook($book);
+      $comment->setIsActive(true);
+      $comment->setCreatedAt((new \DateTimeImmutable('now')));
+      $comment->setAuthor($this->getUser());
+
+      $this->entityManager->persist($comment);
+      $this->entityManager->flush();
+
+      return $this->redirectToRoute('book_show', ['id' => $book->getApiId()]);
     }
 
-    #[Route('/book/{id}', name: 'book', methods: ['GET','POST'])]
-    public function list(Book $book, Request $request): Response
-    {
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class,$comment,[
-            'action' => $this->generateUrl('comment_book', ['id' => $book->getId()]),
-        ]);
-        $form->handleRequest($request);
+    return $this->render('comment/index.html.twig', [
+      'comments' => $book->getComments() ?? [],
+      'commentForm' => $form->createView(),
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $comment->setBook($book);
-            $comment->setIsActive(true);
-            $comment->setCreatedAt((new \DateTimeImmutable('now')));
-            $comment->setAuthor($this->getUser());
+  #[Route('/{id}', name: 'update', methods: ['PUT'])]
+  public function update(#[MapRequestPayload] EditCommentDto $dto, Loader $loader): JsonResponse
+  {
+    $loader->update($dto);
 
-            $this->entityManager->persist($comment);
-            $this->entityManager->flush();
+    return $this->json([]);
+  }
 
-            return $this->redirectToRoute('book_show',['id' => $book->getApiId()]);
-        }
-
-        return $this->render('comment/index.html.twig', [
-            'comments' => $book->getComments() ?? [],
-            'commentForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'update', methods: ['PUT'])]
-    public function update(Request $request): JsonResponse
-    {
-        return $this->json([]);
-    }
-
-    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
-    public function delete(Request $request): JsonResponse
-    {
-        return $this->json([]);
-    }
+  #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+  public function delete(int $id, Loader $loader): JsonResponse
+  {
+    $loader->delete($id);
+    return $this->json([]);
+  }
 }
--- sources/src/Form/CommentType.php
+++ sources/src/Form/CommentType.php
@@ -2,10 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Book;
 use App\Entity\Comment;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,20 +10,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommentType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('content')
-            ->add('Poster',SubmitType::class,[
-                'attr' => ['class' => 'btn button-primary']
-            ])
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('content')
+      ->add('Poster', SubmitType::class, [
+        'attr' => ['class' => 'btn button-primary']
+      ])
+    ;
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Comment::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Comment::class,
+    ]);
+  }
 }
