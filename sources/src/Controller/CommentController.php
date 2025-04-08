e38f7d1aa7a00f94c005352e9964fe57328cc0bc
MrAuGIR <augirard17@gmail.com>
2025-04-08 19:46:29 +0200

    rename loader comment

--- sources/src/Controller/CommentController.php
+++ sources/src/Controller/CommentController.php
@@ -6,7 +6,7 @@ use App\Entity\Book;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Service\Comment\EditCommentDto;
-use App\Service\Comment\Loader;
+use App\Service\Comment\CommentManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,49 +18,49 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/comment', name: 'comment_')]
 class CommentController extends AbstractController
 {
-  public function __construct(
-    private EntityManagerInterface $entityManager
-  ) {}
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
 
-  #[Route('/book/{id}', name: 'book', methods: ['GET', 'POST'])]
-  public function list(Book $book, Request $request): Response
-  {
-    $comment = new Comment();
-    $form = $this->createForm(CommentType::class, $comment, [
-      'action' => $this->generateUrl('comment_book', ['id' => $book->getId()]),
-    ]);
-    $form->handleRequest($request);
+    #[Route('/book/{id}', name: 'book', methods: ['GET', 'POST'])]
+    public function list(Book $book, Request $request): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment, [
+            'action' => $this->generateUrl('comment_book', ['id' => $book->getId()]),
+        ]);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-      $comment->setBook($book);
-      $comment->setIsActive(true);
-      $comment->setCreatedAt((new \DateTimeImmutable('now')));
-      $comment->setAuthor($this->getUser());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setBook($book);
+            $comment->setIsActive(true);
+            $comment->setCreatedAt((new \DateTimeImmutable('now')));
+            $comment->setAuthor($this->getUser());
 
-      $this->entityManager->persist($comment);
-      $this->entityManager->flush();
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
 
-      return $this->redirectToRoute('book_show', ['id' => $book->getApiId()]);
+            return $this->redirectToRoute('book_show', ['id' => $book->getApiId()]);
+        }
+
+        return $this->render('comment/index.html.twig', [
+            'comments' => $book->getComments() ?? [],
+            'commentForm' => $form->createView(),
+        ]);
     }
 
-    return $this->render('comment/index.html.twig', [
-      'comments' => $book->getComments() ?? [],
-      'commentForm' => $form->createView(),
-    ]);
-  }
+    #[Route('/{id}', name: 'update', methods: ['PUT'])]
+    public function update(#[MapRequestPayload] EditCommentDto $dto, CommentManager $manager): JsonResponse
+    {
+        $manager->update($dto);
 
-  #[Route('/{id}', name: 'update', methods: ['PUT'])]
-  public function update(#[MapRequestPayload] EditCommentDto $dto, Loader $loader): JsonResponse
-  {
-    $loader->update($dto);
+        return $this->json([]);
+    }
 
-    return $this->json([]);
-  }
-
-  #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
-  public function delete(int $id, Loader $loader): JsonResponse
-  {
-    $loader->delete($id);
-    return $this->json([]);
-  }
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id, CommentManager $manager): JsonResponse
+    {
+        $manager->delete($id);
+        return $this->json([]);
+    }
 }
---
+++ sources/src/Service/Comment/CommentManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Service\Comment;
+
+use App\Entity\Comment;
+use App\Exception\CommentNotFoundException;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Loader
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private CommentRepository $commentRepository
+    ) {}
+
+    public function load(string|int $id): ?Comment
+    {
+        if (empty($comment = $this->getComment($id))) {
+            return null;
+        }
+        return $comment;
+    }
+
+    public function update(EditCommentDto $dto): void
+    {
+        if (empty($comment = $this->getComment($dto->id))) {
+            throw new CommentNotFoundException("not comment found with this id " . $dto->id);
+        }
+        $comment->setContent($dto->content);
+        $this->em->persist($comment);
+        $this->em->flush();
+    }
+
+    public function delete(string|int $id): void
+    {
+        if (empty($comment = $this->getComment($id))) {
+            throw new CommentNotFoundException("no comment found with this id " . $id);
+        }
+        $this->em->remove($comment);
+        $this->em->flush();
+    }
+
+    public function getComment(string|int $id): Comment
+    {
+        return $this->commentRepository->find($id);
+    }
+}
