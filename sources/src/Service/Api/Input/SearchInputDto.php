aed66d2ec03135f2fe1494d7dcd5e289e78072d7
MrAuGIR <augirard17@gmail.com>
2025-04-09 22:14:17 +0200

    add search by tag

--- sources/src/Service/Api/Input/SearchInputDto.php
+++ sources/src/Service/Api/Input/SearchInputDto.php
@@ -9,9 +9,10 @@ readonly class SearchInputDto
     private const QUERY_PATTERN = "?q=%s&maxResults=%s&key=%s";
 
     public function __construct(
-        #[Assert\NotBlank]
         #[Assert\Length(min: 3, max: 33)]
-        public string  $keywords,
+        public ?string  $keywords,
+
+        public ?string $tag = null,
 
         #[Assert\LessThan(40)]
         public int $l = 10,
@@ -20,12 +21,27 @@ readonly class SearchInputDto
 
     public function buildQuery(string $apiKey): string
     {
+        $searchTerm = $this->keywords ?? $this->getFormattedTag();
         return sprintf(
             self::QUERY_PATTERN,
-            $this->keywords,
+            $searchTerm,
             $this->l,
             $apiKey
         );
+    }
+
+    private function getFormattedTag(): string
+    {
+        if ($this->tag) {
+            return 'subject:' . strtolower($this->tag);
+        }
+
+        return '';
+    }
+
+    public function hasValidSearch(): bool
+    {
+        return !empty($this->keywords) || !empty($this->tag);
     }
 }
 
