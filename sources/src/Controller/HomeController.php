de6c25a03387ab5953e5068ee330896e41e71e5b
MrAuGIR <augirard17@gmail.com>
2025-04-13 16:56:31 +0200

    twig card actu

--- sources/src/Controller/HomeController.php
+++ sources/src/Controller/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use App\Service\Api\Actu;
 use App\Service\Api\GoogleBook;
 use App\Service\Api\Input\ActuInputDto;
 use App\Service\Api\Input\SearchInputDto;
 use App\Service\Api\Output\SearchJsonOutput;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,16 +25,17 @@ class HomeController extends AbstractController
 {
     public function __construct(
         private readonly GoogleBook $googleBook,
-        private readonly Actu $actu
-    )
-    {
-    }
+        private readonly Actu $actu,
+        private readonly EntityManagerInterface $manager
+    ) {}
 
     #[Route('/', name: 'app_home_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(BookRepository $BookRepository): Response
     {
+        $popularBooks = $BookRepository->findMostPopularityBook(9);
 
-        return $this->render('home/index.html.twig',[
+        return $this->render('home/index.html.twig', [
+            'booksPopularity' => $popularBooks,
             'articles' => null
         ]);
     }
@@ -47,11 +50,10 @@ class HomeController extends AbstractController
     #[Route("/actu", name: "app_home_actu", methods: ['POST'])]
     public function getActu(
         #[MapRequestPayload] ActuInputDto $actuInputDto
-    ) : Response
-    {
+    ): Response {
         $articles = $this->actu->list($actuInputDto);
 
-        return $this->render("home/_actu.html.twig",[
+        return $this->render("home/_actu.html.twig", [
             'articles' => $articles["articles"]
         ]);
     }
@@ -62,16 +64,15 @@ class HomeController extends AbstractController
      * @throws RedirectionExceptionInterface
      * @throws ClientExceptionInterface
      */
-    #[Route("/search", name: "app_home_seach", methods: ['GET','POST'])]
+    #[Route("/search", name: "app_home_seach", methods: ['GET', 'POST'])]
     public function search(
         #[MapRequestPayload] SearchInputDto $searchInputDto,
-    ) : JsonResponse
-    {
+    ): JsonResponse {
         $writers = [];
         $collection = $this->googleBook->list($searchInputDto);
 
         return (new SearchJsonOutput($this->renderView('home/_results.html.twig', [
-            'books' => array_slice($collection->getItems(),0,8),
+            'books' => array_slice($collection->getItems(), 0, 8),
             'writers' => $writers
         ])))->getOutput();
     }
